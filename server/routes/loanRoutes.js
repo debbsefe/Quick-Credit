428d56fa7de036ef44a7bc9b698fd6cd0357ac0a
Mamus Eferha <mamuseferha@crenettechlabs.com>
2020-08-19 22:12:47 +0100

    feat(Admin Retreive Single Loan): Implement Admin Retreive Single Loan by id endpoint

--- server/routes/loanRoutes.js
+++ server/routes/loanRoutes.js
@@ -1,13 +1,15 @@
 import express from 'express';
 
-import { loanApply, getAllLoans } from '../controllers/loanController';
+import { loanApply, getAllLoans, retrieveOneLoan } from '../controllers/loanController';
 import { verifyToken, verifyUser } from '../middlewares/verifyAuth';
 
 const router = express.Router();
 
-// users Routes
+// loan Routes
 
 router.post('/loans', verifyUser, loanApply);
 router.get('/loans', verifyToken, getAllLoans);
+router.get('/loans/:id', verifyToken, retrieveOneLoan);
+
 
 export default router;
